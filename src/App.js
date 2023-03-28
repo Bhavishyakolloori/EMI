da7dc949dffa99b602e6d1419fa213a636776476
shashidhar7682 <shashidhar7682@gmail.com>
2023-03-28 13:01:59 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,76 @@
-import logo from './logo.svg';
 import './App.css';
+import Entrepreneurship from './components/Entrepreneurship/Entrepreneurship';
+import EntrepreneurLogin from './components/Entrepreneur/EntrepreneurLogin';
+import EntrepreneurRegister from './components/Entrepreneur/EntrepreneurRegister';
+import MentorLogin from './components/Mentor/MentorLogin';
+import MentorRegister from './components/Mentor/MentorRegister';
+import InvestorLogin from './components/Investor/InvestorLogin';
+import InvestorRegister from './components/Investor/InvestorRegister'; 
+import Home from './components/Home/Home';
+import RouteLayout from './components/RouteLayout';
+import { createBrowserRouter,RouterProvider } from 'react-router-dom';
 
 function App() {
+  const router=createBrowserRouter(
+    [
+      {
+        path:"/",
+        element:<RouteLayout/>,
+        children:[
+          {
+            path:"/",
+            element:<Home/>
+          },
+          {
+            path:"/Mentor",
+            children:[
+              {
+                path:"Login",
+                element:<MentorLogin/>
+              },
+              {
+                path:"Register",
+                element:<MentorRegister/>
+              }
+            ]
+          },
+          {
+            path:"/Entrepreneurship",
+            element:<Entrepreneurship/>,
+          },
+          { 
+            path:"/Entrepreneur",
+            children:[
+              {
+                path:"Login",
+                element:<EntrepreneurLogin/>
+              },
+              {
+                path:"Register",
+                element:<EntrepreneurRegister/>
+              }
+            ]
+          },
+          {
+            path:"/Investor",
+            children:[
+              {
+                path:"Login",
+                element:<InvestorLogin/>
+              },
+              {
+                path:"Register",
+                element:<InvestorRegister/>
+              }
+            ]
+          }
+        ]
+      }
+    ]
+  )
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <RouterProvider router={router}/>
     </div>
   );
 }
